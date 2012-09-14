151f17af2493366ef3e03bf8be761dca078bc105
Angel Alvarez <angel@nan-tic.com>
2012-09-14 10:13:51 +0200

    add images, description an fixes on views

--- __openerp__.py
+++ __openerp__.py
@@ -31,14 +31,78 @@
     "name": "Quality Control",
     "version": "0.1",
     "author": "NaN·tic",
-    "category": "Generic Modules/Others",
+    "category": "Others",
     "description": """
-This module provides a generic infrastructure for quality tests. The idea is that it can be later be reused for doing quality tests in production lots but also in any other areas a company may desire.
+
+Quality Control module for OpenERP, updated to version 6 where
+have added some interesting functionality.
+
+This module is generic, is the basis for integration automatically
+with different models of the application, however allows us to
+any quality control at the different models of the system so
+manual. Over the following weeks introduce other modules
+automate quality control in the production lots or in
+invoices.
+
+Definition:
+
+* Method: The various procedures available to perform qual
+proof.
+* Test: The test to check. We have two types of tests:
+    * Qualitative: The result is a description, color, yes, No..
+    * Quantitative: The result must be within a range.
+* Possible values: The values  choose in qualitative tests.
+* Synonyms: Different names for the same test.
+* References in test: different models of the application to which you can
+pass a quality test.
+* Template: The set of tests to be used in tests!
+
+* Once these values we define the test.
+
+  We have a * generic * test that can be applied to any model as
+  Any company example, shipments, invoices or product, or a * test
+  related *, making it specific to a particular product and that
+  eg apply whenever food is sold or quan creating a
+  batch.
+
+* The * formula *, has been added in version 6.0 to be scored
+the test.
+
+So to define the formula simply use the name of the
+lines as operands. For example: Taking the 3 lines of the template
+named A, B, C respectively can create a formula such that: (A * 100 + B / C) * B ²
+
+
+To give a practical example, this field has been used in some of our
+customers engaged in artificial insemination to calculate the dose
+according to the values resulting quality test passes extraction.
+
+Once these parameters we just pass the test. We create a
+new test, we create a relationship with the model, company sales .. , And pressed
+the "Select Template", choose the test and fills us ĺínieas
+depending on the template chosen.
+
+Now we just fill línieas test. In cases has
+different test methods need only one of the
+combinations test method is correct.
+
+
+We continue the workflow:
+
+    Borarrador -> Confirmed -> Pending approval -> Success
+                                        |
+                                        | -> Failure
 
 Developed for Trod y Avia, S.L.""",
     "depends": [
         'product'
     ],
+    "images": ["images/test.png","images/menu.png","images/metodos.png",
+            "images/plantilla.png","images/pantilla_relacionado.png",
+            "images/prueba_cual.png","images/prueba_cuan.png",
+            "images/pruebas.png","images/reference_model.png",
+            "images/test_conf.png","images/test_ok.png",
+            "images/valores_posibles.png"],
     "demo_xml": [],
     "update_xml": [
         'security/security.xml',
